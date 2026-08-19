584b8120a1755c10dd1104448030e462832a9a3f
mike101944 <fixedmatchese037@gmail.com>
2026-08-20 00:31:08 +0300

    feat:changes made for socket

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 // https://boombet365.com/api
 // Create axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://boombet365.com/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',   
